fd81e5d028a71e21034a812a2da788f10565843d
Jeff Cunningham <jcunningham77@gmail.com>
2016-12-17 20:51:40 -0500

    adding header to lists recyclerview

--- app/src/main/java/com/jeffcunningham/twitterlistviewer_android/ListsAdapter.java
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/ListsAdapter.java
@@ -18,6 +18,10 @@ import java.util.List;
 public class ListsAdapter extends Adapter {
 
     private static final String TAG = "ListsAdapter";
+    private static final int TYPE_LIST_HEADER=0;
+    private static final int TYPE_LIST_SELECTOR =1;
+
+
 
     public void setTwitterLists(List<TwitterList> twitterLists) {
         this.twitterLists = twitterLists;
@@ -27,25 +31,46 @@ public class ListsAdapter extends Adapter {
     private List<TwitterList> twitterLists;
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_selector_view, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        ListSelectorViewHolder vh = new ListSelectorViewHolder(v);
-        return vh;
+
+        View view;
+
+        RecyclerView.ViewHolder viewHolder;
+        if (viewType==TYPE_LIST_HEADER){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.lists_header_view, parent, false);
+            viewHolder = new ListsHeaderViewHolder(view);
+        } else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_selector_view, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+            viewHolder = new ListSelectorViewHolder(view);
+        }
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-        ((ListSelectorViewHolder)holder).setTvListName(twitterLists.get(position).getName());
+        if (position>0){
+            ((ListSelectorViewHolder)holder).setTvListName(twitterLists.get(position-1).getName());
+        }
 
+
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position==0){
+            return TYPE_LIST_HEADER;
+        } else {
+            return TYPE_LIST_SELECTOR;
+        }
     }
 
     @Override
     public int getItemCount() {
         Log.i(TAG, "getItemCount: = ");
-        return twitterLists==null? 0 : twitterLists.size();
+        return twitterLists==null? 0 : twitterLists.size()+1;
     }
 
 
---
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/ListsHeaderViewHolder.java
@@ -0,0 +1,16 @@
+package com.jeffcunningham.twitterlistviewer_android;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by jeffcunningham on 12/17/16.
+ */
+
+public class ListsHeaderViewHolder extends RecyclerView.ViewHolder {
+
+
+    public ListsHeaderViewHolder(View itemView) {
+        super(itemView);
+    }
+}
