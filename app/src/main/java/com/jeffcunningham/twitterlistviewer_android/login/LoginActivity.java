bb5fb11e2b327135fe915b78a889d2e9eb60f99d
Jeff Cunningham <jcunningham77@gmail.com>
2017-01-18 21:35:15 -0500

    use Fragment for Login

--- app/src/main/java/com/jeffcunningham/twitterlistviewer_android/login/LoginActivity.java
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/login/LoginActivity.java
@@ -1,40 +1,23 @@
 package com.jeffcunningham.twitterlistviewer_android.login;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.jeffcunningham.twitterlistviewer_android.R;
 import com.jeffcunningham.twitterlistviewer_android.lists.ListsActivity;
 import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-
     private static final String TAG = "LoginActivity";
-
-
-
-    TwitterLoginButton twitterLoginButton;
-
-
-
-    @BindView(R.id.tvTwitterError)
-    TextView textViewTwitterError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,36 +38,22 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-        twitterLoginButton = (TwitterLoginButton)findViewById(R.id.twitter_login_button);
-        //dont activate until logged in w TLV
-        twitterLoginButton.setEnabled(true);
-        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        LoginFragment loginFragment = new LoginFragment();
+        ft.add(R.id.fragment_container, loginFragment, "LoginFragment");
+        ft.commit();
 
-                TwitterSession session = result.data;
-                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                Log.i(TAG, "success: msg = " + msg);
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-                Intent listsIntent = new Intent(LoginActivity.this, ListsActivity.class);
-                startActivity(listsIntent);
-
-            }
-            @Override
-            public void failure(TwitterException exception) {
-                Log.d("TwitterKit", "Login with Twitter failure", exception);
-                textViewTwitterError.setVisibility(View.VISIBLE);
-                textViewTwitterError.setText("Twitter Login failed due to " + exception.getMessage());
-            }
-        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // Make sure that the twitterLoginButton hears the result from any
-        // Activity that it triggered.
-        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+        FragmentManager fm = getFragmentManager();
+        if (fm != null) {
+            fm.findFragmentByTag("LoginFragment").onActivityResult(requestCode, resultCode, data);
+        }
+        else Log.d("Twitter", "fragment is null");
     }
 
 }
---
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/login/LoginFragment.java
@@ -0,0 +1,80 @@
+package com.jeffcunningham.twitterlistviewer_android.login;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jeffcunningham.twitterlistviewer_android.R;
+import com.jeffcunningham.twitterlistviewer_android.lists.ListsActivity;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by jeffcunningham on 1/18/17.
+ */
+
+public class LoginFragment extends Fragment {
+
+    TwitterLoginButton twitterLoginButton;
+
+    @BindView(R.id.tvTwitterError)
+    TextView textViewTwitterError;
+
+    private static final String TAG = "LoginFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        twitterLoginButton = (TwitterLoginButton) getActivity().findViewById(R.id.twitter_login_button);
+        //don't activate until logged in w TLV
+        twitterLoginButton.setEnabled(true);
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+                TwitterSession session = result.data;
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Log.i(TAG, "success: msg = " + msg);
+                Toast.makeText(getActivity().getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                Intent listsIntent = new Intent(getActivity(), ListsActivity.class);
+                startActivity(listsIntent);
+
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+                textViewTwitterError.setVisibility(View.VISIBLE);
+                textViewTwitterError.setText("Twitter Login failed due to " + exception.getMessage());
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
