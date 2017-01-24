8a7ce6309ae9937037084d5a899b9ee5123e0894
Jeff Cunningham <jcunningham77@gmail.com>
2017-01-24 17:34:59 -0500

    clean up comments in ListsFragment

--- app/src/main/java/com/jeffcunningham/twitterlistviewer_android/lists/ListsFragment.java
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/lists/ListsFragment.java
@@ -2,7 +2,6 @@ package com.jeffcunningham.twitterlistviewer_android.lists;
 
 import android.app.Fragment;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -33,8 +32,6 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static android.content.Context.MODE_PRIVATE;
-
 /**
  * Created by jeffcunningham on 1/18/17.
  */
@@ -55,20 +52,11 @@ public class ListsFragment extends Fragment {
 
     private static final String TAG = "ListsFragment";
 
-//    @Inject
-//    SharedPreferencesRepository sharedPreferencesRepository;
-
-//    @Inject
-//    SharedPreferences sharedPreferences;
-
-
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lists, container, false);
 
-//        ((BaseApplication) getActivity().getApplication()).getPreferencesComponent().inject(this);
         ((ListsActivity) getActivity()).component().inject(this);
 
         ButterKnife.bind(this,view);
@@ -84,44 +72,11 @@ public class ListsFragment extends Fragment {
         listsRecyclerView.setLayoutManager(listsLayoutManager);
         listsAdapter = new ListsAdapter();
         listsRecyclerView.setAdapter(listsAdapter);
-        //--todo inject via Dagger
-//        listsPresenter = new ListsPresenterImpl();
-
+        
         EventBus.getDefault().register(this);
 
         listsPresenter.getListMembershipByTwitterUser();
 
-//        // get current Twitter user's list membership
-//        this.twitterSession = Twitter.getSessionManager().getActiveSession();
-//        TwitterApiClientExtension twitterApiClientExtension = new TwitterApiClientExtension(Twitter.getSessionManager().getActiveSession());
-//        ListOwnershipService listOwnershipService = twitterApiClientExtension.getListOwnershipService();
-//
-//        Call<List<TwitterList>> listMembership= listOwnershipService.listOwnershipByScreenName(twitterSession.getUserName());
-//
-//        listMembership.enqueue(new Callback<List<TwitterList>>(){
-//
-//
-//            @Override
-//            public void success(Result<List<TwitterList>> result) {
-//
-//                for (TwitterList twitterList : result.data){
-//                    Log.i(TAG, "success: twitterList = " + twitterList.getFullName());
-//                }
-//
-//                listsAdapter.setTwitterUserId(twitterSession.getUserId());
-//                listsAdapter.setTwitterLists(result.data);
-//                getDefaultListId(twitterSession.getUserName());
-//
-//            }
-//
-//            @Override
-//            public void failure(TwitterException exception) {
-//
-//                Log.e(TAG, "failure: " + exception.getMessage());
-//                Log.getStackTraceString(exception);
-//
-//            }
-//        });
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -158,72 +113,6 @@ public class ListsFragment extends Fragment {
     }
 
 
-//    private void persistDefaultListId(String alias, String listId, String slug, String listName){
-//        PostDefaultList defaultListBody = new PostDefaultList();
-//        Data defaultListBodyData = new Data();
-//
-//        defaultListBodyData.setAlias(alias);
-//        defaultListBodyData.setListId(listId);
-//        defaultListBodyData.setSlug(slug);
-//        defaultListBodyData.setListName(listName);
-//        defaultListBody.setData(defaultListBodyData);
-//        Log.i(TAG, "persistDefaultListId: listId = " + listId + " slug = " + slug);
-//        Call<DefaultList> postDefaultListCall = apiManager.apiTransactions.postDefaultList(defaultListBody);
-//
-//        postDefaultListCall.enqueue(new retrofit2.Callback<DefaultList>() {
-//            @Override
-//            public void onResponse(Call<DefaultList> call, Response<DefaultList> response) {
-//                Log.i(TAG, "onResponse: Retrofit call to Node post default list succeeded, default list id = " + response.body().getListId());
-//                Log.i(TAG, "onResponse: Retrofit call to Node post default list succeeded, default list slug = " + response.body().getSlug());
-//                Log.i(TAG, "onResponse: Retrofit call to Node post default list succeeded, default list alias = " + response.body().getAlias());
-//
-//                persistDefaultListDataToSharedPreferences(response.body().getSlug(),response.body().getListName());
-//                setDefaultListIdForAdapterLists(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<DefaultList> call, Throwable t) {
-//                Log.e(TAG, "onFailure: " + t.getMessage(), t);
-//
-//            }
-//        });
-//
-//    }
-
-    private void persistDefaultListDataToSharedPreferences(String slug, String listName){
-        Log.i(TAG, "persistDefaultListDataToSharedPreferences: slug = " + slug + ", listName = " + listName);
-        SharedPreferences settings = getActivity().getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("slug",slug);
-        editor.putString("listName",listName);
-        editor.commit();
-
-    }
-
-
-//    private void getDefaultListId(String username) {
-//
-//        Call<DefaultList> getDefaultListCall = apiManager.apiTransactions.getDefaultList(username);
-//
-//
-//        getDefaultListCall.enqueue(new retrofit2.Callback<DefaultList>() {
-//            @Override
-//            public void onResponse(Call<DefaultList> call, Response<DefaultList> response) {
-//                Log.i(TAG, "onResponse: Retrofit call to Node get default list API succeeded, default list id = " + response.body());
-//
-//                persistDefaultListDataToSharedPreferences(response.body().getSlug(),response.body().getListName());
-//                setDefaultListIdForAdapterLists(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<DefaultList> call, Throwable t) {
-//                Log.e(TAG, "onFailure: " + t.getMessage(), t);
-//
-//            }
-//        });
-//
-//
-//    }
 
     private void setDefaultListIdForAdapterLists(DefaultList defaultList){
         for (TwitterList twitterList: listsAdapter.getTwitterLists()){
