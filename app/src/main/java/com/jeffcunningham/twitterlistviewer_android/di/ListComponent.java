fd39e2769357b823141346af60a1d49c70f93326
Jeff Cunningham <jcunningham77@gmail.com>
2017-01-24 18:00:34 -0500

    DI List

--- app/src/main/java/com/jeffcunningham/twitterlistviewer_android/di/ListComponent.java
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/di/ListComponent.java
@@ -1,12 +1,19 @@
 package com.jeffcunningham.twitterlistviewer_android.di;
 
-import android.app.ListActivity;
+import com.jeffcunningham.twitterlistviewer_android.list.TwitterListActivity;
+import com.jeffcunningham.twitterlistviewer_android.list.TwitterListFragment;
+
+import dagger.Component;
 
 /**
  * Created by jeffcunningham on 1/23/17.
  */
 
+@PerActivity
+@Component(dependencies = ApplicationComponent.class, modules = ListModule.class)
 public interface ListComponent {
 
-    void inject(ListActivity activity);
+    void inject(TwitterListActivity activity);
+
+    void inject(TwitterListFragment fragment);
 }
--- app/src/main/java/com/jeffcunningham/twitterlistviewer_android/list/TwitterListFragment.java
+++ app/src/main/java/com/jeffcunningham/twitterlistviewer_android/list/TwitterListFragment.java
@@ -3,17 +3,19 @@ package com.jeffcunningham.twitterlistviewer_android.list;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jeffcunningham.twitterlistviewer_android.R;
+import com.jeffcunningham.twitterlistviewer_android.util.Logger;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.TwitterListTimeline;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,13 +37,19 @@ public class TwitterListFragment extends ListFragment {
 
     private static final String TAG = "TwitterListFragment";
 
+    @Inject
+    Logger logger;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_twitter_list, container, false);
-        Log.i(TAG, "onCreateView: getArguments().getString(\"slug\",\"\") = " + getArguments().getString("slug",""));
+        ((TwitterListActivity) getActivity()).component().inject(this);
+        logger.info(TAG, "onCreateView: getArguments().getString(\"slug\",\"\") = " + getArguments().getString("slug",""));
         this.slug = getArguments().getString("slug","");
         this.listName = getArguments().getString("listName","");
+
+
 
         ButterKnife.bind(this,view);
         return view;
@@ -51,18 +59,13 @@ public class TwitterListFragment extends ListFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-
-
-
-
         this.twitterSession = Twitter.getSessionManager().getActiveSession();
         this.alias = twitterSession.getUserName();
 
         tvListName.setText("@"+this.alias+"/"+ this.listName);
 
-        Log.i(TAG, "onCreate: slug = " + slug + ", alias = " + alias);
-        Log.i(TAG, "onViewCreated: this.slug = " + this.slug);
+        logger.info(TAG, "onCreate: slug = " + slug + ", alias = " + alias);
+        logger.info(TAG, "onViewCreated: this.slug = " + this.slug);
 
         final TwitterListTimeline userTimeline = new TwitterListTimeline.Builder()
                 .slugWithOwnerScreenName(slug,alias)
